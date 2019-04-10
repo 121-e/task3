158af2de7ee65a773e002e7c5972256f8e517b36
121-e <121-e@mail.ru>
2019-04-10 19:04:28 +0300

    1

---
+++ src/task3.java
@@ -0,0 +1,9 @@
+public class task3 {
+    public static void main(String[] arg) {
+        int x = 3,y=0;
+        for (int i = 0; i<10;i++) {
+            System.out.println(y+"  ");
+            y = y+3;
+        }
+    }
+}
---
+++ src/task4.java
@@ -0,0 +1,12 @@
+public class task4 {
+    public static void main(String[] args) {
+        for (int i=1; i<10; i++) {
+            for (int k=1; k<11; k++) {
+                int r = i*k;
+                System.out.print(r+" ");
+            }
+            System.out.println();
+        }
+    }
+
+}
